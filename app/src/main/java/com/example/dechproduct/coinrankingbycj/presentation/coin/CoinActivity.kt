5f60c11e4fc793a968a680afe6c9a3d148f43113
Dech <61090003@kmitl.ac.th>
2021-07-15 23:26:44 +0700

    Add Dependency injection

--- app/src/main/java/com/example/dechproduct/coinrankingbycj/presentation/coin/CoinActivity.kt
+++ app/src/main/java/com/example/dechproduct/coinrankingbycj/presentation/coin/CoinActivity.kt
@@ -1,9 +1,10 @@
-package com.example.dechproduct.coinrankingbycj
+package com.example.dechproduct.coinrankingbycj.presentation.coin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dechproduct.coinrankingbycj.R
 
-class MainActivity : AppCompatActivity() {
+class CoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
